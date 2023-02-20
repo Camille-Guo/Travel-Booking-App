c6c099206b9ba79f77a7e52952beb2018cff615d
Camille-Guo <zibin_guo@hotmail.com>
2023-02-20 16:41:44 -0500

    total

---
+++ Data/Migrations/20230206042710_changeAirportModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelBooking.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeAirportModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
